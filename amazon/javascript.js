d6e4e5669254a50e98ca4776f5e564352b14a963
Vinbond <144142621+Vinbond@users.noreply.github.com>
2023-10-07 15:36:20 +0530

    Add files via upload

---
+++ amazon/javascript.js
@@ -0,0 +1,23 @@
+/*let namasteBtn=document.getElementsByClassName('main-img');
+namasteBtn.addEventListener('click', ShowMsg);
+
+function ShowMsg(){
+  alert("namste World!");
+} */
+var fruits=prompt('what you want');
+switch(fruits){
+case 'apple':
+  document.writeln('30 rupees');
+  break;
+  case 'apple':
+    document.writeln('30 rupees');
+    break;
+    case 'apple':
+      document.writeln('30 rupees');
+      break;
+      default:
+        document.writeln(`${fruits} new try`);
+
+}
+document.writeln('out off stock try somthing new');
+
